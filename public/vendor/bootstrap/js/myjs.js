6afd88ee569abfbdf428ebb94cf5250ccd71adec
Kinasu <ashtoreet@gmail.com>
2016-07-02 11:53:23 +0300

    menu+

--- public/vendor/bootstrap/js/myjs.js
+++ public/vendor/bootstrap/js/myjs.js
@@ -3,3 +3,9 @@ $(document).ready(function()  {
     alert("Javacript подключен");
 });
 
+$(function() {
+	$('.nav-toggle').on('click', function(){
+		$('.main-navigation').toggleClass('open');
+
+	});
+});
